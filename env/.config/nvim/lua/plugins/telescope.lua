8dc02bb63bad599b1439ae6248d80e7a324b382d
Jose Naylor <jose.naylor@finxact.com>
2025-02-21 20:05:25 -0500

    added harpoon, change telescope vim bindings, changed wezterm to make way for a bg switch script

--- env/.config/nvim/lua/plugins/telescope.lua
+++ env/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,30 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>dd', builtin.diagnostics, { desc = 'Telescope LSP diagnostics' })
-		end
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>tf", function()
+				builtin.find_files({ hidden = true })
+			end, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>tg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>td", builtin.diagnostics, { desc = "Telescope LSP diagnostics" })
+		end,
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require("telescope").setup {
+			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
-						}
-					}
-				}
-			}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
 }
--- env/.config/wezterm/wezterm.lua
+++ env/.config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ config.default_cursor_style = 'BlinkingBar'
 --config.macos_window_background_blur = 20
 config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
 --config.window_background_opacity = 0.50
-config.window_background_image = '/Users/jose.naylor/dev-env/env/.config/wezterm/nightcity-bright.png'
+config.window_background_image = '/Users/jose.naylor/.config/wezterm/bg.png'
 config.window_background_image_hsb = {
   -- Darken the background image by reducing it to 1/3rd
   brightness = 0.1,
