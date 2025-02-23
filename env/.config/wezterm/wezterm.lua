016a4e2b6c7f475344a61341e9d2ff9df08393b4
Jose Naylor <jose.c.naylor@gmail.com>
2025-02-22 20:00:04 -0500

    Changed wezterma and change-bg to use $HOME, made other scripts runnable, and changed tmux-sessionizer to account for different laptop file structure

--- env/.config/wezterm/wezterm.lua
+++ env/.config/wezterm/wezterm.lua
@@ -1,16 +1,17 @@
 local wezterm = require "wezterm"
 local config = {}
 local action = wezterm.action
+local home = os.getenv( "HOME" )
 
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'Hack Nerd Font'
+--config.font = wezterm.font 'Hack Nerd Font'
 
 config.default_cursor_style = 'BlinkingBar'
 
 --config.macos_window_background_blur = 20
 config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
 --config.window_background_opacity = 0.50
-config.window_background_image = '/Users/jose.naylor/.config/wezterm/bg.png'
+config.window_background_image = home .. '/.config/wezterm/bg.png'
 config.window_background_image_hsb = {
   -- Darken the background image by reducing it to 1/3rd
   brightness = 0.1,
