318bee66e6e52759ee8af10d1c1803b4d23d0afd
Jose Naylor <jose.c.naylor@gmail.com>
2025-07-27 01:08:59 -0400

    added terraform lsp

--- env/.config/nvim/lua/plugins/lsp-config.lua
+++ env/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls" },
+				ensure_installed = { "lua_ls", "gopls", "terraformls" },
 			})
 		end,
 	},
@@ -22,6 +22,9 @@ return {
 				capabilities = capabilities,
 			})
 			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.terraformls.setup({
 				capabilities = capabilities,
 			})
 
